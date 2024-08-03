4fb4a5408b5f3751bd78bf85c849689fd62056e9
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-08-02 21:29:59 -0600

    Agregando interfaz

---
+++ Operacion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioInterfaces
+{
+    public class Operacion : IOperaciones
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+
+        public Operacion() { }
+
+        //Implementando metodos abstractos
+
+        public double Potencia()
+        {
+            return Math.Pow(Numero1, Numero2);
+        }
+
+
+        public double Logaritmo()
+        {
+            return Math.Log(Numero1, Numero2);
+        }
+
+
+        public double AreaCuadrado()
+        {
+            return Numero1 * Numero2;
+        }
+
+        public double Modulo()
+        {
+            return Numero1 % Numero2;
+        }
+
+        public double AreaTriangulo()
+        {
+            return Numero1 * Numero2 / 2;
+        }
+
+
+        public void Imprimir()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("------Resultado de operaciones---------");
+            Console.WriteLine($"{Numero1} elevado a la {Numero2} es: {Potencia()}");
+            Console.WriteLine($"El logaritmo de {Numero1} en base {Numero2} es: {Logaritmo()}");
+            Console.WriteLine($"El area del cuadrado con base {Numero1} y altura {Numero2} es: {AreaCuadrado()}");
+            Console.WriteLine($"El residuo de {Numero1} dividido por {Numero2} es: {Modulo()}");
+            Console.WriteLine($"El area de un triangulo donde base = {Numero1} y altura = {Numero2} es: {AreaTriangulo()}");
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,34 @@ namespace EjercicioInterfaces
     {
         static void Main(string[] args)
         {
+            Operacion operacion = new Operacion();
 
+            //Validando que el formato de los atributos sean numeros
+            try
+            {
+                Console.Write("Ingrese el primer numero para las operaciones:");
+                operacion.Numero1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Ingrese el segundo numero para las operaciones:");
+                operacion.Numero2 = Convert.ToDouble(Console.ReadLine());
+
+                if (operacion.Numero2 != 0)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("El resultado de las operaciones es:");
+                    operacion.Imprimir();
+
+                }
+                else
+                {
+                    Console.WriteLine("El segundo numero no puede ser un 0 por una de las operaciones");
+                }
+
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
 
         }
     }
